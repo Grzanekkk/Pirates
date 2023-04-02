b0b366161defc40e1db8560934636d431ef22558
Jan Puto <tofukles@gmail.com>
2023-04-02 12:04:49 +0200

    Initial commit

---
+++ Source/Pirates/Pirates.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pirates.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pirates, "Pirates" );
