984678733fd7833bc27a1b107bda89f87357299c
Jan Puto <tofukles@gmail.com>
2023-09-11 01:06:35 +0200

    New Wind Particles

--- Source/Pirates/Subsystems/WeatherSubsystem.cpp
+++ Source/Pirates/Subsystems/WeatherSubsystem.cpp
@@ -8,4 +8,28 @@ void UWeatherSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	Super::Initialize(Collection);
 
 	UE_LOG(LogTemp, Warning, TEXT("WeathreSubsystem Initialized."));
+	GetWorld()->OnWorldBeginPlay.AddUObject(this, UWeatherSubsystem::WorldBeginPlay);
+}
+
+void UWeatherSubsystem::WorldBeginPlay()
+{
+	FTimerDelegate TryChangeWindDirectionDelegate;
+	TryChangeWindDirectionDelegate.BindUObject(this, UWeatherSubsystem::TryChangeWindDirection);
+	GetWorld()->GetTimerManager().SetTimer(TryChangeWindDirectionTimer, TryChangeWindDirectionDelegate, TryChangeWindDirectionInterval, true);
+}
+
+void UWeatherSubsystem::TryChangeWindDirection()
+{
+	DesiredWindRotation = FMath::RandRange(0, 360);
+
+	FTimerDelegate TryChangeWindDirectionDelegate;
+	TryChangeWindDirectionDelegate.BindUObject(this, UWeatherSubsystem::TryChangeWindDirection);
+	GetWorld()->GetTimerManager().SetTimer(AddWindRotationTimer, AddWindRotationInterval, true;
+
+	WorldWindDirection
+}
+
+void UWeatherSubsystem::AddWindDirection()
+{
+
 }
--- Source/Pirates/Subsystems/WeatherSubsystem.h
+++ Source/Pirates/Subsystems/WeatherSubsystem.h
@@ -16,4 +16,36 @@ class PIRATES_API UWeatherSubsystem : public UWorldSubsystem
 	
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	UPROPERTY()
+	float TryChangeWindDirectionInterval = 5.f;
+
+	UPROPERTY()
+	float AddWindRotationInterval = 0.2f;
+
+	UPROPERTY()
+	float AddWindRotationRate = 0.1f;
+
+protected:
+	UFUNCTION()
+	void WorldBeginPlay();
+
+	UFUNCTION()
+	void TryChangeWindDirection();
+
+	UFUNCTION()
+	void AddWindDirection();
+
+	UPROPERTY()
+	float WorldWindDirection = 0;
+
+	UPROPERTY()
+	float DesiredWindRotation = 0;
+
+	UPROPERTY()
+	FTimerHandle TryChangeWindDirectionTimer;
+
+	UPROPERTY()
+	FTimerHandle AddWindRotationTimer;
+
 };
